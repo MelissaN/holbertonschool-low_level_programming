9487b97e12de592c068bafcdf40db5dbf44c6d32
MelissaN <majajachiu@yahoo.com>
2018-01-18 01:52:35 +0000

    reformat all on one line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %zu bytes(s)\n", sizeof(char));
-	printf("Size of an int: %zu bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int))
-		;
-	printf("Size of a float: %zu bytes(s)\n", sizeof(float));
-	return (0);
-
+printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+printf("Size of an int: %zu bytes(s)\n", sizeof(int));
+printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %zu bytes(s)\n", sizeof(float));
+return (0);
 }
