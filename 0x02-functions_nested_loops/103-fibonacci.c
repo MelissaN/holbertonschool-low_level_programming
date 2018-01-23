942decb80f992d7aab77427c01e0996bc2ac67d6
MelissaN <majajachiu@yahoo.com>
2018-01-23 22:05:40 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of even Fionacci sequence up to 4,000,000
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int sum_of_evens = 0;
+	unsigned long int a;
+	unsigned long int b;
+	unsigned long int sum = 1;
+
+	a = 1;
+	b = 1;
+
+	while (b < 4000000)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		if ((sum <= 4000000) && (sum % 2 == 0))
+			sum_of_evens += sum;
+	}
+	printf("%lu \n", sum_of_evens);
+
+	return (0);
+}
