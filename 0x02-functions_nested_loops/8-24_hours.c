9d1efd88b7cb707b43d7cafff7c5072605ea5eba
MelissaN <majajachiu@yahoo.com>
2018-01-23 20:20:28 +0000

    rewrote code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,27 +7,22 @@
 
 void jack_bauer(void)
 {
-	int hour0;
-	int hour1;
-	int minute0;
-	int minute1;
+	int hours = 0;
+	int min = 0;
 
-	for (hour0 = 0; hour0 <= 2; hour0++)
+	while (hours < 24)
 	{
-		for (hour1 = 0; hour1 < 4; hour1++)
+		while (min < 60)
 		{
-			for (minute0 = 0; minute0 <= 5; minute0++)
-			{
-				for (minute1 = 0; minute1 <= 9; minute1++)
-				{
-					_putchar('0' + hour0);
-					_putchar('0' + hour1);
-					_putchar(':');
-					_putchar('0' + minute0);
-					_putchar('0' + minute1);
-					_putchar('\n');
-				}
-			}
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
 		}
+		min = 0;
+		hours++;
 	}
 }
