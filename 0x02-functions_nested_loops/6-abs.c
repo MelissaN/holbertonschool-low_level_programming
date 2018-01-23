e3d30da09ac6fba527a23b133cfd2b96f6b0b28d
MelissaN <majajachiu@yahoo.com>
2018-01-23 19:16:17 +0000

    change (n >= 0) to (n > 0)

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,17 @@
 #include "holberton.h"
 
 /**
- * _abs - computer absolute value of integer
+ * _abs - compute absolute value of integer
  * @n: integer argument
  * Return: absolute value
  */
 
 int _abs(int n)
 {
-	if (n >= 0)
-	{
+	if (n > 0)
 		return(n);
-	}
 	else if (n < 0)
-	{
-		n *= -1;
-		return(n);
-	}
+		return(-n);
+
 	return (0);
 }
