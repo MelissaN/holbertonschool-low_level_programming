090ba99922dc2dec81ff5cf41d7526ee7bbbb85e
MelissaN <majajachiu@yahoo.com>
2018-01-24 05:40:03 +0000

    fix arithmetic

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,7 @@ void print_times_table(int n)
 					else if (product >= 100)
 					{
 						_putchar('0' + (product / 100));
-						_putchar('0' + ((product / 10) - 10));
+						_putchar('0' + ((product / 10) % 10));
 						_putchar('0' + (product % 10));
 					}
 				}
