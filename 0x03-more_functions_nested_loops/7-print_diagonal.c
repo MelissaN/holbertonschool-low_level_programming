8170376c15a99dcdb5fab1c13ba176da00a5a3a3
MelissaN <majajachiu@yahoo.com>
2018-01-26 04:44:21 +0000

    add spaces to slant diagonal line

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,16 @@
 void print_diagonal(int n)
 {
 	int draw;
+	int space;
 
 	if (n > 0)
 	{
 		for (draw = 1; draw <= n; draw++)
 		{
+			for (space = 1; space < draw; space++)
+				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
-
 }
