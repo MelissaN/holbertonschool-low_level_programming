2da8fed2e2f14775c8a99358361ea12e628141cc
MelissaN <majajachiu@yahoo.com>
2018-01-26 20:59:54 +0000

    replace Fizz with FizzBuzz

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("Fizz");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
