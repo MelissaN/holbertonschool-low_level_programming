10ce48be37f51fc45886bef3d5c83e40c1aa9be9
MelissaN <majajachiu@yahoo.com>
2018-01-26 04:43:19 +0000

    remove space after last digit

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,19 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	printf("1 2");
+
+	for (i = 3; i <= 100; i++)
 	{
-		if (i < 3)
-			printf("%d ", i);
+
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
 		else
-		{
-			if ((i % 3 == 0) && (i % 5 == 0))
-				printf("FizzBuzz ");
-			else if (i % 3 == 0)
-				printf("Fizz ");
-			else if (i % 5 == 0)
-				printf("Buzz ");
-			else
-				printf("%d ", i);
-		}
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
