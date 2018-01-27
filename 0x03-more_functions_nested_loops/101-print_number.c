8217ef8985d34975f9f61bd8b7e1ebc7d41855f1
MelissaN <majajachiu@yahoo.com>
2018-01-27 04:35:54 +0000

    add condition for 0

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -10,21 +10,26 @@ void print_number(int n)
 	unsigned int tens, digit, positive = n;
 	double t_beg = 1;
 
-	if (n < 0)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		positive = n * -1;
-		_putchar('-');
-	}
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
 
-	while (t_beg < positive)
-		t_beg *= 10;
-	tens = t_beg / 10;
+		while (t_beg < positive)
+			t_beg *= 10;
+		tens = t_beg / 10;
 
-	while (tens >= 1)
-	{
-		digit = positive / tens;
-		_putchar(digit + '0');
-		positive = (positive - (tens * digit));
-		tens /= 10;
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
 	}
 }
