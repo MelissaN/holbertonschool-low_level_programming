4a1c6f7a5d6a9143bd91987bd9b37a6fa480c735
MelissaN <majajachiu@yahoo.com>
2018-01-30 23:20:56 +0000

    restrict output to fit within strlen

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,36 @@
 #include "holberton.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
  * puts2 - prints every second character of a string, followed by a new line
  * @str: string to print
  */
 
 void puts2(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-		str++;
-	}
+	int i;
 
+	for (i = 1;  i <= _strlen(str); i += 2)
+	{
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
