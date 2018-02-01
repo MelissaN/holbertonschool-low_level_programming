e6ebe6867ad85ca33e01b3a75ea09a90c945d5a3
MelissaN <majajachiu@yahoo.com>
2018-02-01 05:59:48 +0000

    refactor code

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -38,6 +38,27 @@ int idx_num_starts(char *s)
 }
 
 /**
+ * find_sign - determine if integer is negative
+ * @s: integer
+ * Return: integer 1 or -1
+ */
+int find_sign(char *s)
+{
+	int negatives = 0, i = 0, sign = 1;
+
+	while (i < (idx_num_starts(s)))
+	{
+		if (s[i++] == '-')
+			negatives++;
+	}
+
+	if (negatives % 2 != 0)
+		sign = -1;
+
+	return (sign);
+}
+
+/**
  * _atoi - convert string to int
  * @s: string to convert
  * Return: integer
@@ -47,40 +68,33 @@ int _atoi(char *s)
 {
 
 	int idx_digit_starts = (idx_num_starts(s));
-	int negatives = 0, sign = 1; /* hold positive or negative */
+	int sign;
 	int digits_to_print = 0;
 	int t = 1, i, num = 0;
 	int digit = (idx_num_starts(s));
 
-	if (idx_digit_starts < 0)
+	if (idx_digit_starts < 0) /* if no digits found, exit program */
 		return (0);
 
-	i = 0;
-	while (i < idx_digit_starts)
-	{
-		if (s[i++] == '-')
-			negatives++;
-	}
-
-	if (negatives % 2 != 0)
-		sign = -1;
+	sign = find_sign(s);
 
 	while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9')
-	       && (idx_digit_starts <= _strlen(s)))
+	       && (idx_digit_starts <= _strlen(s))) /* count digits to print */
 	{
 		digits_to_print += 1;
 		idx_digit_starts++;
 	}
 
 	i = 1;
-	while (i < digits_to_print)
+	while (i < digits_to_print) /* find powers of ten to multiply places */
 	{
 		t *= 10;
 		i++;
 	}
 
-	for (i = digit; i < (digit + digits_to_print); i++)
-	{       num += (s[i] - '0') * t;
+	for (i = digit; i < (digit + digits_to_print); i++) /* calculate num */
+	{
+		num += (s[i] - '0') * t;
 		t /= 10;
 	}
 
