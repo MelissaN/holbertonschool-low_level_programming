6417fbfa9fde56e4b2a5c331bb85b565573b9174
MelissaN <majajachiu@yahoo.com>
2018-02-06 05:28:20 +0000

    initial commit

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - set value of pointer to a char
+ * @s: variable of type pointer
+ * @to: char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
