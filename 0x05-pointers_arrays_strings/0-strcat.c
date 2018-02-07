f183b220e2fd7f2d2c77178203b4bfd62989d9c3
MelissaN <majajachiu@yahoo.com>
2018-02-07 02:24:15 +0000

    add pointer notation alternative

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -25,3 +25,25 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
+
+/* another method using pointer notation
+
+char *_strcat(char *dest, char *src)
+{
+        int i = 0;
+	int j = 0;
+
+	while (*(dest + i))
+	   i++;
+
+	while (*(src + j))
+	{
+	   *(dest+i) = *(src+j);
+	   i++;
+	   j++;
+	}
+	*(dest+i) = '\0';
+
+	return (dest);
+}
+*/
