fc8e5152f024bcfe2d28ee09d050d76afb9a656b
MelissaN <majajachiu@yahoo.com>
2018-02-07 03:03:39 +0000

    add pointer arithmetic version

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -22,3 +22,20 @@ char *_strncpy(char *dest, char *src, int n)
 
 	return (dest);
 }
+
+/* this method in pointer arithmetic
+
+char *_strncpy(char *dest, char *src, int n)
+{
+  int i;
+
+  for (i = 0; i < n && *(src + i); i++)
+      *(dest + i) = *(src + i);
+  while (i < n)
+  {
+      *(dest + i) = '\0';
+      i++;
+  }
+  return (dest);
+}
+*/
