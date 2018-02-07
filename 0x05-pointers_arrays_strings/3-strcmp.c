0217d92cf2a5c1dbeba7366f6ade892adb5e098a
MelissaN <majajachiu@yahoo.com>
2018-02-07 03:07:40 +0000

    make consistent pointer arithemtic notation

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (*(s1 + i) == *(s2 + i) && *(s1 + i) != '\0')
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
 		i++;
 
-	if (s2[i] != '\0')
-		return (s1[i] - s2[i]);
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
 	else
 		return (0);
 }
