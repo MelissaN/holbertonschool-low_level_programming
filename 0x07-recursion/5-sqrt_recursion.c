37976d6ce10f2986c5f3276be40bbf5a08ce30f1
MelissaN <majajachiu@yahoo.com>
2018-02-08 04:08:28 +0000

    attempt another method to avoid large data types

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,41 +1,21 @@
 #include "holberton.h"
 
 /**
- * increment_root - increment potential root by one to retest if square root
- * @num: number to find square root of
+ * find_root - find square root of n, starting from the smallest possible, 0
+ * @n: n
  * @root: test this root
  * Return: natural square root, or -1 if not natural root
  */
 
-int increment_root(unsigned int num, int root)
+int find_root(int n, int root)
 {
-	unsigned long product = root * root;
-
-	if (product < num)
-		return (increment_root(num, root + 1));
-	else if (product == num)
-		return (root);
-	else
+	if (root * root > n)
 		return (-1);
-}
 
-/**
- * decrement_root - decrement potential root by one to retest if square root
- * @num: number to find square root of
- * @root: test this root
- * Return: natural square root, or -1 if not natural root
- */
-
-int decrement_root(unsigned int num, int root)
-{
-	unsigned long product = root * root;
-
-	if (product > num)
-		return (decrement_root(num, root - 1));
-	else if (product == num)
+	if (root * root == n)
 		return (root);
-	else
-		return (-1);
+
+	return (find_root(n, root + 1));
 }
 
 /**
@@ -46,16 +26,8 @@ int decrement_root(unsigned int num, int root)
 
 int _sqrt_recursion(int n)
 {
-	unsigned int num = n;
-	int root = n / 2;
-	unsigned long product = root * root;
+	if (n < 0)
+		return (-1);
 
-	if (product < num)
-		return (increment_root(num, root + 1));
-	else if (product > num)
-		return (decrement_root(num, root - 1));
-	else if (product == num)
-		return (root);
-
-	return (-1);
+	return (find_root(n, 0));
 }
