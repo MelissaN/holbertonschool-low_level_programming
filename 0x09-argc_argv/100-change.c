a95addd8354436c7ff13c3bb8c9df5b437bb9b0c
MelissaN <majajachiu@yahoo.com>
2018-02-11 04:54:46 +0000

    rewrite condition for negative input

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -20,14 +20,14 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	/* convert string to int and calculate coins */
-	n = atoi(argv[1]);
-
-	if (argv[1][0] == '-' || n == 0)
+	if (argv[1][0] == '-')
 	{
 		printf("0\n");
 		return (0);
 	}
+
+	/* convert string to int and calculate coins */
+	n = atoi(argv[1]);
 
 	coins += n / 25;
 	n = n % 25;
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 	n = n % 5;
 	coins += n / 2;
 	n = n % 2;
-	coins += 1;
+	coins += n / 1;
 
 	printf("%d\n", coins);
 	return (0);
