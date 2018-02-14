a7e6089acd8fd8fdd6845759f9296cc931753e86
MelissaN <majajachiu@yahoo.com>
2018-02-14 07:15:32 +0000

    revise comments

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -17,12 +17,12 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0) /* validate input */
 		return (NULL);
 
-	grid = malloc(height * sizeof(int *)); /*allocate memory for ptr array*/
+	grid = malloc(height * sizeof(int *)); /*allocate memory for rows*/
 
 	if (grid == NULL) /* validate memory */
 		return (NULL);
 
-	for (i = 0; i < height; i++) /*allocate memory for 2D part of array */
+	for (i = 0; i < height; i++) /*allocate memory for columns of each row*/
 	{
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL) /* validate memory */
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
-		for (j = 0; j < width; j++)
+		for (j = 0; j < width; j++) /* set array values to 0 */
 			grid[i][j] = 0;
 	}
 
