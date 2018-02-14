d7b942085152960e11e84bf5aba1cce1e122465c
MelissaN <majajachiu@yahoo.com>
2018-02-14 03:09:46 +0000

    add more prototypes

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -6,5 +6,7 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 
 #endif
