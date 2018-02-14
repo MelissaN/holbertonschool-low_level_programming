12be9c48f9b8b65d7c4b2b677371c44ddd0c1677
MelissaN <majajachiu@yahoo.com>
2018-02-14 00:12:06 +0000

    replace sizeof with length

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -12,17 +12,23 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	int i = 0, j = 0;
+	int len = 0, i = 0, j = 0;
 
         if (s1 == NULL)
                 s1 = "";
         if (s2 == NULL)
                 s2 = "";
-        concat = malloc(sizeof(s1) + sizeof(s2)); /*determine memory allocation*/
+	while (*(s1 + i))
+		len++, i++;
+	while (*(s2 + j))
+		len++, j++;
+
+        concat = malloc(sizeof(char) + len); /*determine memory alloc*/
 
 	if (concat == NULL) /* validate memory */
 		return (NULL);
 
+	i = 0, j = 0;
 	while (*(s1 + i)) /* concatenate */
 	{
 		*(concat + i) = *(s1 + i);
