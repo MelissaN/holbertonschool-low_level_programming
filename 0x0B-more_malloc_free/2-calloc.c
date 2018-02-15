0815c17f053c5bda4490d8aaa9a228515c3f7ef1
MelissaN <majajachiu@yahoo.com>
2018-02-15 23:19:42 +0000

    initial commit

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _calloc - allocate memory and set all values to 0
+ * @nmemb: size
+ * @size: sizeof(datatype)
+ * Return: pointer to calloc'd string
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	unsigned int i; /* match unsigned arguments */
+
+	if (nmemb <= 0 || size <= 0) /* validate input */
+		return (NULL);
+
+	/* allocate memory and check if error */
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+
+	/* set allocated memory values to 0 */
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+
+	return ((void *)ptr);
+}
