7aa4d0d154e27540ac7bcef75cf7b95ccc34fdb1
MelissaN <majajachiu@yahoo.com>
2018-02-16 05:58:59 +0000

    account for original null pointer

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -2,10 +2,10 @@
 #include "holberton.h"
 
 /**
- * realloc - allocate memory and set all values to 0
- * @ptr - pointer to the memory previously allocated (malloc(old_size))
- * @old_size - size previously allocated
- * @new_size - new size to reallocate
+ * _realloc - allocate memory and set all values to 0
+ * @ptr: pointer to the memory previously allocated (malloc(old_size))
+ * @old_size: size previously allocated
+ * @new_size: new size to reallocate
  * Return: pointer to reallocated memory
  */
 
@@ -23,11 +23,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size) /* return ptr if reallocating same old size */
 		return (ptr);
 
+	if (ptr == NULL) /* malloc new size if ptr is originally null */
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		else
+			return (p);
+	}
+
 	p = malloc(new_size); /* malloc and check error */
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < new_size; i++) /* fill in values from previous ptr */
+	/* fill up values up till minimum of old or new size */
+	for (i = 0; i < old_size && i < new_size; i++)
 		*((char *)p + i) = *((char *)ptr + i);
 
 	return (p);
