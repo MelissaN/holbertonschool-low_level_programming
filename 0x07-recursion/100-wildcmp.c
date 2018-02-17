91441962ffd3340102ed17f483ec01114da649dc
MelissaN <majajachiu@yahoo.com>
2018-02-17 21:42:19 +0000

    initial commit

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,42 @@
+/**
+ * substring_match - check if a substring after wildcard matches s1
+ * @s1: one string
+ * @s2: one string
+ * @after_wldcd: placeholder for position right after wildcard
+ * Return: 1 if matched, 0 if not
+ */
+
+int substring_match(char *s1, char *s2, char *after_wldcd)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (substring_match (s1, s2 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
+	if (*s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+	else
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+}
+
+/**
+ * wildcmp - compare if string with wildcard mattches
+ * @s1: one string
+ * @s2: one string
+ * Return: 1 if matched, 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
+	else
+		return (0);
+}
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -71,4 +71,6 @@ int is_prime_number(int n);
 
 int is_palindrome(char *s);
 
+int wildcmp(char *s1, char *s2);
+
 #endif
