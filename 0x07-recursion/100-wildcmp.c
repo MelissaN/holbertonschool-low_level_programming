b8e9caab0cb32229eee004ed3b57259152f216a1
MelissaN <majajachiu@yahoo.com>
2018-02-17 21:44:03 +0000

    fix betty style

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -11,7 +11,7 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == '\0' && *s2 == '*')
-		return (substring_match (s1, s2 + 1, s2 + 1));
+		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == '\0' && *s2 != '\0')
 		return (0);
 	if (*s2 == '*')
