2ad5d45991be02d3f129238d4ee40217bc49750c
MelissaN <majajachiu@yahoo.com>
2018-02-20 04:17:07 +0000

    fix betty style

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -12,8 +12,12 @@ void print_dog(struct dog *d)
 	if (d == NULL) /* validate if d initiated correctly */
 		return;
 
-	(d->name == NULL) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
+	(d->name == NULL) ? printf("Name: (nil)\n")
+		: printf("Name: %s\n", d->name);
+
 	printf("Age: %f\n", d->age);
-	(d->owner == NULL) ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
+
+	(d->owner == NULL) ? printf("Owner: (nil)\n")
+		: printf("Owner: %s\n", d->owner);
 
 }
--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,8 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
+/**
+ * struct dog - structure for dogs
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
 struct dog
 {
 	char *name;
