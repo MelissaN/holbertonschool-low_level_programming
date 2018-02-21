aee3edbfb5154ccbaad259a674c1f0352e5b318b
MelissaN <majajachiu@yahoo.com>
2018-02-21 23:38:11 +0000

    add input validation

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * print_name - execute a function given as a parameter
  * @name: parameter that function ptr needs
@@ -7,5 +9,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
