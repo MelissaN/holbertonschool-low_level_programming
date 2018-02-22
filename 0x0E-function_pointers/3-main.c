4bd1a45ee80619d007133b33cd7388515d36a5c4
MelissaN <majajachiu@yahoo.com>
2018-02-22 04:01:46 +0000

    fix betty style

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -29,8 +29,8 @@ int main(int argc, char *argv[])
 	n2 = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-	if ((argv[2][1] != '\0') || (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
-			  && argv[2][0] != '/' && argv[2][0] != '%'))
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
+	     argv[2][0] != '/' && argv[2][0] != '%') || (argv[2][1] != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
