3bdc77545bf78fc868172b655b2dad6d9ec71286
MelissaN <majajachiu@yahoo.com>
2018-02-24 09:10:48 +0000

    add struct declaration for file#3

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -7,4 +7,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct dt
+{
+	char *letter;
+	void (*func)(va_list);
+} datatype;
+
 #endif
