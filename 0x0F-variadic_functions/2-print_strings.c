7c038976f782b7aaf9ea6b6057d650468f251986
MelissaN <majajachiu@yahoo.com>
2018-02-24 17:14:58 +0000

    (nil) instead of nil

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			s = va_arg(valist, char *);
 			if (s == NULL)
-				printf("nil");
+				printf("(nil)");
 			else
 				printf("%s", s);
 
