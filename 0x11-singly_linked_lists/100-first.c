320591c86d1814d26dfa9be9dae16b5521d2e528
MelissaN <majajachiu@yahoo.com>
2018-03-02 05:04:56 +0000

    reorder attribute

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -5,9 +5,7 @@
  * Note: made possible by prior declaration of "__attribute__ ((constructor))"
  */
 
-void execute_before_main(void) __attribute__ ((constructor));
-
-void execute_before_main(void)
+void __attribute__ ((constructor)) execute_before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
