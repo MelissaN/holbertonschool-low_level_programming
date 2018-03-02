5bc72e8dd94f7bb90498e702fb6309dbe5bc98e4
MelissaN <majajachiu@yahoo.com>
2018-03-02 04:59:42 +0000

    add challenge

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * execute_before_main - executes this function before main function
+ * Note: made possible by prior declaration of "__attribute__ ((constructor))"
+ */
+
+void execute_before_main(void) __attribute__ ((constructor));
+
+void execute_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
