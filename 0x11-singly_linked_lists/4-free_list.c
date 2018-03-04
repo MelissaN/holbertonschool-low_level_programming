4fa2fee6df3a33230fcc057c1fd6cadcd0c3c91c
MelissaN <majajachiu@yahoo.com>
2018-03-04 20:08:08 +0000

    another method without temp variable

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -7,16 +7,27 @@
 
 void free_list(list_t *head)
 {
-	list_t *ptr;
-
-	if (head == NULL) /* account for no linked list */
-		return;
-
-	while (head != NULL) /* have ptr keep track of head node and free */
+	while (head != NULL)
 	{
-		ptr = head;
-		head = head->next; /* move to next node while ptr frees prior */
-		free(ptr->str); /* free malloced strings */
-		free(ptr);
+		free(head->str);
+		free(head);
+		head = head->next;
 	}
 }
+
+/**
+ * another way using temp pointer
+ *
+ *      list_t *ptr;
+ *
+ *	if (head == NULL) // account for no linked list
+ *		return;
+ *
+ *	while (head != NULL) // have ptr keep track of head node and free
+ *	{
+ *		ptr = head;
+ *		head = head->next; // move to next node while ptr frees prior
+ *		free(ptr->str); // free malloced strings
+ *		free(ptr);
+ *	}
+ */
