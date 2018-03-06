033e1d7429387909cae8f569422de919bccec144
MelissaN <majajachiu@yahoo.com>
2018-03-06 19:10:21 +0000

    switch pointer variables for accuracy

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -15,7 +15,7 @@ void free_listint2(listint_t **head)
 	while (*head != NULL) /* have ptr keep track of head node and free */
 	{
 		ptr = *head;
-		*head = (*head)->next; /* move to next node while ptr frees prior */
+		*head = ptr->next; /* move to next node while ptr frees prior */
 		free(ptr);
 	}
 	*head = NULL;
