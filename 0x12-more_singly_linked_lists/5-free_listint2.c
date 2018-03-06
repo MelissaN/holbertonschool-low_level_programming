ca3c62e02ca59ea397a23776f75087765909316d
MelissaN <majajachiu@yahoo.com>
2018-03-06 19:15:47 +0000

    account for no ptr

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (*head == NULL) /* account for no linked list */
+	if (*head == NULL || !head) /* account for no linked list and no ptr */
 		return;
 
 	while (*head != NULL) /* have ptr keep track of head node and free */
