a0608e3a193e46b15ec2a2d254f91f500b7853db
MelissaN <majajachiu@yahoo.com>
2018-03-06 19:53:02 +0000

    manipulate tmp ptr instead of real *head

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	/* account for deleting beginning node */
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		*head = tmp->next;
 		free(tmp);
 		return (1);
 	}
