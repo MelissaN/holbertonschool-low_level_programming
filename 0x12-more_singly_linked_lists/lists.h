60dd1977284eaf45a76577bde5c8b5e4d251ca90
MelissaN <majajachiu@yahoo.com>
2018-03-07 05:21:39 +0000

    add more function prototypes

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -32,5 +32,7 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
