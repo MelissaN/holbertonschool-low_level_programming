30b5430f5e0a0efa9dee0ec9697cd94a965c5795
MelissaN <majajachiu@yahoo.com>
2018-03-07 05:59:04 +0000

    finds loop, not yet printing or freeing

---
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find loop in linked list
+ * @head: pointer to head pointer of linked list
+ * Return: address of node where loop starts
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *turtle, *hare;
+
+	/* determine if loop exists by seeing if turtle and hare meets */
+	/* start them at head, move turtle one node and hare two nodes */
+	turtle = hare = head;
+
+	while (turtle != NULL && hare != NULL)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+
+		if (turtle == hare)
+		{
+			/* start turtle at head and hare at address they met */
+			/* move turtle and hare one node to find loop origin */
+			turtle = head;
+			while (turtle != hare)
+			{
+				turtle = turtle->next;
+				hare = hare->next;
+			}
+			return(turtle);
+		}
+	}
+
+	return (NULL);
+}
