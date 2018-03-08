ccd35f3187be12f0c21dcd0dd7b60f71f8d7cacb
MelissaN <majajachiu@yahoo.com>
2018-03-08 04:41:32 +0000

    add comment

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		tmp = tmp->next;
 		i++;
-		if (tmp == NULL)
+		if (tmp == NULL) /* account for index out of range */
 			return (-1);
 	}
 
