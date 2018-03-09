9d4759ecb89757f8b38a30bf731e50cae41d9f86
MelissaN <majajachiu@yahoo.com>
2018-03-09 06:41:05 +0000

    initial commit

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * set_bit - set bit to 1 at given index
+ * @n: number
+ * @index: index within binary number
+ * Return: 1 if success, or -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int max_bits;
+	int mask;
+
+	/* validate index is not out of range */
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
+	/* create mask with 1 at index (...00100...) to work on that index */
+	mask = 1 << index;
+	*n = (*n | mask);
+
+	return (1);
+}
