d2a3055b71a843368cba33142c80e657fdfe1d14
MelissaN <majajachiu@yahoo.com>
2018-03-09 08:03:47 +0000

    initial commit

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - determine how many bits to flip to get from one num to another
+ * @n: number
+ * @m: number2
+ * Return: how many bits differ
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int diff_bits = 0;
+	unsigned long int difference;
+
+	/* Xor both nums to show bit 1 if different bits */
+	difference = n ^ m;
+
+	/* keep shifting difference to right and tallying the ones up */
+	do
+	{
+		diff_bits += (difference & 1);
+		difference >>= 1;
+	}
+	while
+		(difference > 0);
+
+	return (diff_bits);
+}
