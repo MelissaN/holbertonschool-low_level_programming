a7f4b9cca16f7a006216c197c450db4623c25b12
MelissaN <majajachiu@yahoo.com>
2018-03-09 06:21:25 +0000

    initial commit

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * get_bit - get bit at index
+ * @n: number
+ * @index: index within binary number
+ * Return: bit 0 or 1, or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+	unsigned int max_bits;
+
+	/* validate index is not out of range */
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
+	/* shift number index places right to find bit */
+	bit = ((n >> index) & 1);
+
+	return (bit);
+}
