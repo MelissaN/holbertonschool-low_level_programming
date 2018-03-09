e560b6b3a8bc86bf4bd4a07be0e3b43e366d6739
MelissaN <majajachiu@yahoo.com>
2018-03-09 07:32:44 +0000

    initial commit

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - clear bit to 0 at given index
+ * @n: number
+ * @index: index within binary number
+ * Return: 1 if success, or -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int max_bits;
+	int mask;
+
+	/* validate index is not out of range */
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
+	/* create mask with 0 at index (...11011...) to work on that index */
+	mask = ~(1 << index);
+	*n = (*n & mask);
+
+	return (1);
+}
