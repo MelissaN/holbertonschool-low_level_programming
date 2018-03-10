8c54bc75c9653875db7543fa135c91bee69dc044
MelissaN <majajachiu@yahoo.com>
2018-03-10 06:39:55 +0000

    add alternative method

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -8,27 +8,59 @@
 unsigned int binary_to_uint(const char *b)
 {
 
-	int len = 0, pow = 1;
-	unsigned int sum = 0, error = 0;
+	int len = 0, i;
+	unsigned int sum = 0;
 
 	if (b == NULL)
-		return (error);
+		return (sum);
 
-	while (b[len] != '\0') /* find string length */
+	/* find string length */
+	while (b[len] != '\0')
 		len++;
 	len -= 1;
 
-	while (len >= 0) /* iterate from back of string */
+	/* iterate string and if '1' then multiply by power of 2 */
+	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
+	i = 0;
+	while (b[i])
 	{
-		if ((b[len] != '0') && (b[len] != '1'))
-			return (error);
-
-		if (b[len] == '1') /* add appropriate power of 2 if '1' */
-			sum += pow;
-
-		pow *= 2; /* update power of 2 */
+		if (b[i] == '1')
+			sum += (1 * (1 << len));
+		i++;
 		len--;
 	}
 
 	return (sum);
 }
+
+/* alternative method not using bitwise but a power of 2
+ *
+ *
+ * unsigned int binary_to_uint(const char *b)
+ *{
+ *
+ *	int len = 0, pow = 1;
+ *	unsigned int sum = 0, error = 0;
+ *
+ *	if (b == NULL)
+ *		return (error);
+ *
+ *	while (b[len] != '\0') // find string length
+ *		len++;
+ *	len -= 1;
+ *
+ *	while (len >= 0) // iterate from back of string
+ *	{
+ *		if ((b[len] != '0') && (b[len] != '1'))
+ *			return (error);
+ *
+ *		if (b[len] == '1') // add appropriate power of 2 if '1'
+ *			sum += pow;
+ *
+ *		pow *= 2; // update power of 2
+ *		len--;
+ *	}
+ *
+ *	return (sum);
+ *}
+ */
--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -32,15 +32,16 @@ void print_binary(unsigned long int n)
 	}
 }
 
-/* another method without creating long mask 100... */
-/* this while method starts at left beginning of binary and uses mask 1 */
-
-	/* while (len >= 0) */
-	/* { */
-	/* 	if ((n >> len) & 1) */
-	/* 		_putchar('1'); */
-	/* 	else */
-	/* 		_putchar('0'); */
-
-	/* 	len--; */
-	/* } */
+/* another method without creating a lengthy mask (100...)
+ * this while method starts at left beginning of binary and uses mask 1
+ *
+ *  while (len >= 0)
+ *  {
+ *  	if ((n >> len) & 1)
+ *  		_putchar('1');
+ *  	else
+ *  		_putchar('0');
+ *
+ *  	len--;
+ *  }
+ */
