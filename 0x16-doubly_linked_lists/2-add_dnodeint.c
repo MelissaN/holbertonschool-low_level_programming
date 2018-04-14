30d5f750f0462f4335f71861b17fd86cb3fdd041
MelissaN <majajachiu@yahoo.com>
2018-04-14 21:42:22 +0000

    undo modularity

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -2,34 +2,6 @@
 #include <stdlib.h>
 
 /**
- * one_node_list_ - add one node to empty doubly linked list
- * @head: pointer to head of list
- * @new: pointer to new node
- */
-void one_node_list_(dlistint_t **head, dlistint_t **new)
-{
-	*head = *new;
-	(*new)->next = NULL;
-	(*new)->prev = NULL;
-}
-
-/**
- * create_node_ - malloc and set data of new node
- * @n: node data
- * Return: address of new node
- */
-dlistint_t *create_node_(const int n)
-{
-	dlistint_t *new;
-
-	new = malloc(sizeof(struct dlistint_s));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	return (new);
-}
-
-/**
  * add_dnodeint - add node to front of doubly linked list
  * @head: pointer to head of list
  * @n: node data
@@ -39,13 +11,21 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
 	/* malloc and set new node data */
-	new = create_node_(n);
+	new = malloc(sizeof(struct dlistint_s));
+	if (!new)
+		return (NULL);
+	new->n = n;
 
 	/* account for empty linked list */
 	if (*head == NULL)
 	{
-		one_node_list_(head, &new);
+		*head = new;
+		new->next = NULL;
+		new->prev = NULL;
 		return (new);
 	}
 
@@ -54,6 +34,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	(*head)->prev = new;
 	*head = new;
-
 	return (new);
 }
