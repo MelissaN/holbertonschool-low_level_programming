4c2165b29e7863d9de69c2263df162133e281d7a
MelissaN <majajachiu@yahoo.com>
2018-04-14 21:47:13 +0000

    undo modularity

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,34 +2,6 @@
 #include <stdlib.h>
 
 /**
- * one_node_list - add one node to empty doubly linked list
- * @head: pointer to head of list
- * @new: pointer to new node
- */
-void one_node_list(dlistint_t **head, dlistint_t *new)
-{
-	*head = new;
-	new->next = NULL;
-	new->prev = NULL;
-}
-
-/**
- * create_node - malloc and set data of new node
- * @n: node data
- * Return: address of new node
- */
-dlistint_t *create_node(const int n)
-{
-	dlistint_t *new;
-
-	new = malloc(sizeof(struct dlistint_s));
-	if (!new)
-		return (NULL);
-	new->n = n;
-	return (new);
-}
-
-/**
  * add_dnodeint_end - add node to end of doubly linked list
  * @head: pointer to head of list
  * @n: node data
@@ -40,12 +12,17 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new, *last;
 
 	/* malloc and set new node data */
-	new = create_node(n);
+	new = malloc(sizeof(struct dlistint_s));
+	if (!new)
+		return (NULL);
+	new->n = n;
 
 	/* account for empty linked list */
 	if (*head == NULL)
 	{
-		one_node_list(head, new);
+		*head = new;
+		new->next = NULL;
+		new->prev = NULL;
 		return (new);
 	}
 
