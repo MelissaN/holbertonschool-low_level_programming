4ab58e3fbb1d660532f5ec82a694c6d5df5188b5
MelissaN <majajachiu@yahoo.com>
2018-08-07 23:06:38 +0000

    add header file

---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
