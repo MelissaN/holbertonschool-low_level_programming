d2ded9b87e5beef56b1663f2aa6c69a576b9443a
MelissaN <majajachiu@yahoo.com>
2018-08-07 23:07:01 +0000

    add linear and binary search function

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search linearly
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return -1;
+
+	i = 0;
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1D-search_algorithms/1-binary.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+
+/**
+ * print_arr - helper func to print array everytime array is halved
+ * @array: array
+ * @l: left index of original array
+ * @r: right index of original array
+ */
+void print_arr(int *array, size_t l, size_t r)
+{
+	size_t i = 0;
+
+	printf("Searching in array: ");
+	for (i = l; i <= r; i++)
+	{
+		if (i != r)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
+}
+
+/**
+ * binary_search - search mids
+ * @array: given array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: index at which value's found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid = 0;
+	size_t l = 0;
+	size_t r = size - 1;
+
+	if (!array)
+		return -1;
+
+	while (l <= r)
+        {
+                print_arr(array, l, r);
+                mid = (l + r)/2;
+                if (array[mid] == value)
+                        return (mid);
+                else if (array[mid] > value)
+                        r = mid - 1;
+                else
+                        l = mid + 1;
+        }
+	return (-1);
+}
