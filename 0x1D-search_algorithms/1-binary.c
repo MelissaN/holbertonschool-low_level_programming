1fe00becc0bc0a61e21fcfadc879c8c69f101a68
MelissaN <majajachiu@yahoo.com>
2018-08-08 18:35:05 +0000

    betty style fix

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -34,18 +34,18 @@ int binary_search(int *array, size_t size, int value)
 	size_t r = size - 1;
 
 	if (!array)
-		return -1;
+		return (-1);
 
 	while (l <= r)
-        {
-                print_arr(array, l, r);
-                mid = (l + r)/2;
-                if (array[mid] == value)
-                        return (mid);
-                else if (array[mid] > value)
-                        r = mid - 1;
-                else
-                        l = mid + 1;
-        }
+	{
+		print_arr(array, l, r);
+		mid = (l + r) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			r = mid - 1;
+		else
+			l = mid + 1;
+	}
 	return (-1);
 }
