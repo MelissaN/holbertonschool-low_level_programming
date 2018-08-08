f15b74df82f43435f39b52942abbfcdf6745c436
MelissaN <majajachiu@yahoo.com>
2018-08-08 22:11:34 +0000

    add function prototype to header

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -2,8 +2,10 @@
 #define SEARCH_ALGOS
 
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
